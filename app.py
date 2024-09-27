4a9bfb5cb83868629afa1b095aecc38be939bd58
RefOoSami001 <159632003+RefOoSami001@users.noreply.github.com>
2024-09-27 17:50:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,6 @@ def verification_code_finder():
         return redirect(url_for('login'))
     if 'user' in session:
         if request.method == 'POST':
-            phpsessid = request.form['phpsessid']
             numbers = request.form['numbers'].split()
 
             total_success = 0
@@ -209,7 +208,7 @@ def verification_code_finder():
             codes = {}
 
             for number in numbers:
-                code = get_panel_code(phpsessid, number)
+                code = get_panel_code(number)
                 status = 'Failed'
                 if code:
                     total_success += 1
@@ -234,69 +233,20 @@ def verification_code_finder():
 
 
 
-def get_panel_code(phpsessid, number):
-    # Sanitize the phone number to ensure it starts with a '+'
-    if not number.startswith('+'):
-        number = f'+{number}'
-    cookies = {
-        'PHPSESSID': phpsessid,
-        'cf_clearance': 'q215qKk3TnaopVq7.TJ9uqwoDUhikIk3c99bZcGv9oQ-1727272832-1.2.1.1-AQnFZ3fusgPSgLJpnQ3rHy_6wJphr6B5uNvj.6CjKH86WQEWXsZ1aWfVLPeAuN21UWIYuAkeFpOLXbmuIY7tA5xncJH0b.ZZo2jfSLpx8g8FAuPpfgVF0lYPmOwRVg0kMztaoEXpw9qBl2Uv3tCbatHRqTaMZNZ_MHkbVfHQ_N_XzxYO_Ewy7TqvDvGhswEi3XmJ_b_JAYcM.dLUg3N5TvU.rgl9UupdTPbvfA8zroHs8qr6TfVNmquyZpbI43.1_ef67oGM1RsUYl_513eHNJL4aV3XrhI05f6Q6KbSF0abBVvC2xvxXghzDhtzAVFc7zkzQiyICiKc2iuN9w5Okyp7QslCjDiKjCFMSdmwujPA9_oFcP335lI.cvpo4zy3rWDLqfcEaXBZ2_8uTkLM_L1RT6_yTTEI0uLFB9kUytPGCaptS32b2jzM.NbRcC9R',
-    }
-
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
-        'cache-control': 'max-age=0',
-        'content-type': 'application/x-www-form-urlencoded',
-        'origin': 'https://mediateluk.com',
-        'priority': 'u=0, i',
-        'referer': 'https://mediateluk.com/sms/index.php?opt=shw_sts_today',
-        'sec-ch-ua': '"Not)A;Brand";v="99", "Microsoft Edge";v="127", "Chromium";v="127"',
-        'sec-ch-ua-arch': '"x86"',
-        'sec-ch-ua-bitness': '"64"',
-        'sec-ch-ua-full-version': '"127.0.2651.98"',
-        'sec-ch-ua-full-version-list': '"Not)A;Brand";v="99.0.0.0", "Microsoft Edge";v="127.0.2651.98", "Chromium";v="127.0.6533.100"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-model': '""',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-ch-ua-platform-version': '"15.0.0"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
-    }
-
-    params = {
-        'opt': 'shw_sts_today_det',
-    }
-
-    data = {
-        'ddi': number,
-        'oad': 'CloudOTP',
-    }
-    try:
-        response = requests.post('https://mediateluk.com/sms/index.php', params=params, cookies=cookies, headers=headers, data=data)
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Use CSS selectors to target the 'MESSAGE BODY' column in the table
-        verification_msg = soup.select_one('table.table.table-head-bg-warning tr.table_line_even td:nth-child(6)')
-
-        if verification_msg:
-            # Extract the text of the verification message
-            message_text = verification_msg.text
-            # Use a regular expression to find the digits in the message
-            verification_code = re.search(r'\d+', message_text)
-            
-            if verification_code:
-                return verification_code.group()
-            else:
-                return None
+def get_panel_code(number):
+    response = requests.get(f'http://51.89.151.134/crapi/viewstats?token=%20Q1FXNEVBgmBJf5Bzd2aEW0qWUV1_VWRHZYdxa0l2YIR7l3VTVnI=&record=200&filternum={number}').json()
+    if response['status'] == 'success':
+        message_text = response['data'][0]['message']
+        # Use a regular expression to find the digits in the message
+        verification_code = re.search(r'\d+', message_text)
+        
+        if verification_code:
+            return verification_code.group()
         else:
             return None
-    except:
+    else:
         return None
+
 
 if __name__ == '__main__':
     init_db()
